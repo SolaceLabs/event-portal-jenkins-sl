7dcf004142060f9880f80aba8596c4457c3ba1c3
Alfonso Chacón <alfonso.chacon@solace.com>
2025-05-16 20:50:58 -0600

    + push var

--- vars/solaceEventPortalPushToRuntime.groovy
+++ vars/solaceEventPortalPushToRuntime.groovy
@@ -1,11 +1,7 @@
 #!/usr/bin/env groovy
 
-import com.cloudbees.groovy.cps.NonCPS
 import com.solace.ep.jenkins.Python
 
-def script
-
-@NonCPS
 def call(Map config) {
 	return new Python(this).pushApplicationToRuntime("","","","")
 	

16e6ccfda3c62536b3af955e5499f70e463a3563
Alfonso Chacón <alfonso.chacon@solace.com>
2025-05-17 00:08:37 -0600

    * vars

--- vars/solaceEventPortalPushToRuntime.groovy
+++ vars/solaceEventPortalPushToRuntime.groovy
@@ -3,7 +3,7 @@
 import com.solace.ep.jenkins.Python
 
 def call(Map config) {
-	def value = config."${token}"
+	def value = config.token
 	sh "echo the value is ${value}"
 	return new Python(this).pushApplicationToRuntime("","","","")
 	

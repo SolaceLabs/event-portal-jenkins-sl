60e9dfcff2af01312571daf47bdb52bee6423f7d
Alfonso Chacón <alfonso.chacon@solace.com>
2025-05-16 19:43:51 -0600

    * install dependencies scripts

--- resources/python/dependencies/install-dependencies.sh
+++ resources/python/dependencies/install-dependencies.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
+cd python
+cd dependencies
 cd lib
-python -m pip install pip-25.1.1-py3-none-any.whl
-python -m pip install certifi-2025.4.26-py3-none-any.whl
-python -m pip install charset_normalizer-3.4.2-py3-none-any.whl
-python -m pip install idna-3.10-py3-none-any.whl
-python -m pip install urllib3-2.4.0-py3-none-any.whl
-python -m pip install requests-2.32.3-py3-none-any.whl
+python3 -m pip install pip-25.1.1-py3-none-any.whl
+python3 -m pip install certifi-2025.4.26-py3-none-any.whl
+python3 -m pip install charset_normalizer-3.4.2-py3-none-any.whl
+python3 -m pip install idna-3.10-py3-none-any.whl
+python3 -m pip install urllib3-2.4.0-py3-none-any.whl
+python3 -m pip install requests-2.32.3-py3-none-any.whl
+cd ..
+cd ..
 cd ..
 
